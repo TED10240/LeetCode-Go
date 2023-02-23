5c618ace39ea24f1336e26d0706a165caf65d8ae
TED10240 <classic10240@gmail.com>
2023-02-23 22:59:40 +0800

    Add 0086

---
+++ src/LeetCode/0086.Partition-List/86.Partition List.go
@@ -0,0 +1,26 @@
+package leetcode
+
+import "structures"
+
+type ListNode = structures.ListNode
+
+func partition(head *ListNode, x int) *ListNode {
+	beforeHead := &ListNode{Val: 0, Next: nil}
+	before := beforeHead
+	afterHead := &ListNode{Val: 0, Next: nil}
+	after := afterHead
+
+	for head != nil {
+		if head.Val < x {
+			before.Next = head
+			before = before.Next
+		} else {
+			after.Next = head
+			after = after.Next
+		}
+		head = head.Next
+	}
+	after.Next = nil
+	before.Next = afterHead.Next
+	return beforeHead.Next
+}
